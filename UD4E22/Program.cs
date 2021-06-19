806d341326aa4e675b698c844586f933cfb086ad
Martina <martinaescorial9@gmail.com>
2021-06-19 15:52:34 +0200

    Add project files.

---
+++ UD4E22/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UD4E22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String contraseña = "12345678";
+
+            int intentos = 3;
+
+            //Esta variable booleana, nos controlara que en caso de que acierte la condicion cambie
+            Boolean acierto = false;
+
+            String password;
+            for (int i = 0; i < intentos && !acierto; i++)
+            {
+                password = "Introduce una contraseña";
+
+                //Comprobamos si coincide, no usamos ==, usamos el metodo equals
+                if (password==(contraseña))
+                {
+                    Console.WriteLine("Enhorabuena, acertaste");
+                    acierto = true;
+                }
+            }
+
+        }
+    }
+}
